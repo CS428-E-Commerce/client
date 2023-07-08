15d59a340d9f78d1e4a4f92ef8a9f52dfa8baa82
hai.truong <haiquytruong.dev@gmail.com>
2023-07-08 08:42:12 +0700

    [f] Tutor profile page
    - Change project name to "vinglish"
    - Setup font family
    - Setup media query mixins
    - Create Footer, Navbar components
    - Modify NonAuthorizedLayout
    - Create mocked up TutorProfile page
    - Add responsive

---
+++ customer/src/components/Footer/index.jsx
@@ -0,0 +1,67 @@
+import { NavLink } from "react-router-dom";
+
+import classes from "./styles.module.scss";
+
+import {
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+  YoutubeIcon,
+} from "assets/images/icons";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.footerContent}>
+        <div className={classes.footerColumn}>
+          <h3 className={classes.footerLogo}>Vinglish</h3>
+          <div className={classes.footerList}>
+            <span className={classes.footerItem}>
+              Empower yourself in Vietnamese and use it as your native language
+            </span>
+          </div>
+        </div>
+        <div className={classes.footerColumn}>
+          <h4 className={classes.footerHeader}>For Students</h4>
+          <div className={classes.footerList}>
+            <NavLink to="/">Vinglish Blog</NavLink>
+            <NavLink to="/">Questions and Answers</NavLink>
+            <NavLink to="/">Student discount</NavLink>
+            <NavLink to="/">Referral program</NavLink>
+            <NavLink to="/">Test your Vietnamese for free</NavLink>
+            <NavLink to="/">Test your vocab</NavLink>
+            <NavLink to="/">Free Vietnamese courses</NavLink>
+          </div>
+        </div>
+        <div className={classes.footerColumn}>
+          <h4 className={classes.footerHeader}>For Tutors</h4>
+          <div className={classes.footerList}>
+            <NavLink to="/">Become an online tutor</NavLink>
+            <NavLink to="/">Become a coach online</NavLink>
+          </div>
+        </div>
+        <div className={classes.footerColumn}>
+          <h4 className={classes.footerHeader}>About Us</h4>
+          <div className={classes.footerList}>
+            <NavLink to="/">Who we are</NavLink>
+            <NavLink to="/">How it works</NavLink>
+            <NavLink to="/">Vinglish certificates</NavLink>
+            <NavLink to="/">Work at Vinglish</NavLink>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.footerCopyRight}>
+        <div>© 2023 Vinglish. All Rights Reserved | Privacy</div>
+        <div className={classes.icons}>
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+          <YoutubeIcon />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- customer/src/components/Layouts/NonAuthorizedLayout/index.jsx
+++ customer/src/components/Layouts/NonAuthorizedLayout/index.jsx
@@ -1,15 +1,22 @@
 import { memo } from "react";
 import { withRouter } from "react-router-dom";
-import { Container } from "@mui/material";
+
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+
 import classes from "./styles.module.scss";
 
 const NonAuthorizedLayout = memo(props => {
   const { children } = props;
 
   return (
-    <div className={classes.container}>
-      <Container>{children}</Container>
-    </div>
+    <>
+      <div className={classes.container}>
+        <Navbar />
+        {children}
+      </div>
+      <Footer />
+    </>
   );
 });
 
