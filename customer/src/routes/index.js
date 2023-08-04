4514a9ccea5277e8b8697d16c0826116a2d02d22
quangvust201 <quangvust201@gmail.com>
2023-08-04 22:35:43 +0700

    basic frame

--- customer/src/routes/index.js
+++ customer/src/routes/index.js
@@ -4,6 +4,7 @@ import LoginPage from "pages/Login";
 import StudentRegisterPage from "pages/StudentRegister";
 import TutorProfilePage from "pages/TutorProfile";
 import TutorRegisterPage from "pages/TutorRegister";
+import MyCourse from "pages/MyCourse";
 
 const privateRoutes = [
   // { path: "/", component: HomePage }
@@ -16,6 +17,7 @@ const publicRoutes = [
   { path: "/tutor-signup", component: TutorRegisterPage },
   { path: "/find-tutors", component: FindTutorsPage },
   { path: "/tutor/:id", component: TutorProfilePage },
+  { path: "/my-course", component: MyCourse },
 ];
 
 export { privateRoutes, publicRoutes };
