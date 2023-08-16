2972c3ab345f43f67856afe601920105f118f7f7
Son Truong <sontruong@pixelcent.com>
2023-08-17 03:06:44 +0700

    Fix bugs

--- administrator/src/routes/index.js
+++ administrator/src/routes/index.js
@@ -1,17 +1,17 @@
 import CoursesPage from "pages/Courses";
 import TutorsPage from "pages/Tutors";
 import LoginPage from "pages/Login";
-import TutorDetailPage from "pages/TutorDetail";
 import SignupPage from "pages/Signup";
 import { Redirect } from "react-router";
-import CourseDetailPage from "pages/CourseDetail";
+// import TutorDetailPage from "pages/TutorDetail";
+// import CourseDetailPage from "pages/CourseDetail";
 
 const privateRoutes = [
   { path: "/", exact: true, component: () => <Redirect to="/tutors" /> },
   { path: "/tutors", component: TutorsPage },
-  { path: "/tutor/:tutorId", component: TutorDetailPage },
   { path: "/courses", component: CoursesPage },
-  { path: "/courses/:courseId", component: CourseDetailPage },
+  // { path: "/tutor/:tutorId", component: TutorDetailPage },
+  // { path: "/courses/:courseId", component: CourseDetailPage },
 ];
 
 const publicRoutes = [
