9259dacefe0a27cee17f57c2ab58af5cb4eefb02
hai.truong <haiquytruong.dev@gmail.com>
2023-08-27 15:11:17 +0700

    My Courses Navigation Fix

--- customer/src/components/BasicMenu/index.jsx
+++ customer/src/components/BasicMenu/index.jsx
@@ -57,7 +57,7 @@ const UserMenu = memo(({ title }) => {
   };
 
   const navigateMyCourses = () => {
-    dispatch(push("/change-password"));
+    dispatch(push("/my-course"));
   };
 
   const logout = () => {
